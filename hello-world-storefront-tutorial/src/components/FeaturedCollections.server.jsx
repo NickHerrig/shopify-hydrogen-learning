785b7788150a43c9bb16dbd87932db9fdf55b41c
nickherrig <neherrig@gmail.com>
2022-08-14 20:56:00 -0500

    Add product collection page
      - utilize file-based routing system
      - query products in collection by handle
      - Seo tags and shopify analytics components
      - Fetch products query with variants gql

--- hello-world-storefront-tutorial/src/components/FeaturedCollections.server.jsx
+++ hello-world-storefront-tutorial/src/components/FeaturedCollections.server.jsx
@@ -11,7 +11,9 @@ export default function FeaturedCollections() {
         )
 
     return (
+
         <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+          
           <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
             Collections
           </h2>
--- hello-world-storefront-tutorial/src/routes/index.server.tsx
+++ hello-world-storefront-tutorial/src/routes/index.server.tsx
@@ -2,11 +2,12 @@
 import { Suspense } from "react";
 
 import FeaturedCollections from "../components/FeaturedCollections.server"
-import { Layout } from              "../components/Layout.server"
+import { Layout } from "../components/Layout.server"
 
 export default function Home() {
   return (
     <Layout>
+
       <Suspense>
         <FeaturedCollections />
       </Suspense>
